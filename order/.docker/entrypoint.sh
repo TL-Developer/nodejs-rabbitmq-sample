2671947a453b1a37de7c3325501a724bbeb5fdd5
Tiago Lima <tiago.jlima.developer@gmail.com>
2023-06-20 13:53:29 -0300

    start

---
+++ order/.docker/entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd /home/node/nest
+
+npm install
+npm run start:dev
---
+++ order/src/app.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Get, Post, Req } from '@nestjs/common';
+import { AppService } from './app.service';
+import RabbitmqServer from './rabbitmq-server';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Post('nest')
+  async nest(@Req() request: Request) {
+    const server = new RabbitmqServer('amqp://admin:admin@rabbitmq:5672');
+    await server.start();
+    await server.publishInQueue('express', JSON.stringify(request.body));
+    await server.publishInExchange('amq.direct','rota2', JSON.stringify(request.body));
+    return request.body;
+  }
+}
